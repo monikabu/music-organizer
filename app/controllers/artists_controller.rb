dadc29a576a4d2a5c75dde0db184ea5918b4e0eb
Monika Burdzy <monika-burdzy@wp.pl>
2014-04-29 21:16:19 +0200

    update artists view

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,8 @@
 class ArtistsController < ApplicationController
-  
+  respond_to :html, :xml, :json
   def index
-    
+    @user = User.find(current_user.id)
+    @artist = @user.artists.all
   end
   def new
     @user = User.find(current_user.id)
@@ -12,6 +13,31 @@ class ArtistsController < ApplicationController
     @user = User.find(current_user.id)
     @artist =  @user.artists.create(artist_params)
     redirect_to new_artist_album_path(@artist)
+  end
+  
+   def edit
+    @user = User.find(current_user.id)
+    @artist = @user.artists.find(params[:id])
+    respond_with(@artist)
+  end
+  
+  def update
+    @user = User.find(current_user.id)
+    @artist = @user.artists.find(params[:id])
+    
+    if 
+      @artist.update(artist_params)
+      redirect_to user_artists_path(@user)
+    else
+      render "edit"
+    end
+  end
+   
+  def destroy
+    @user = User.find(current_user.id)
+    @artist = @user.artists.find(params[:id])
+    @artist.destroy
+    redirect_to user_artists_path(@user)
   end
   
   private
